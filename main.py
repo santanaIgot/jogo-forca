27ae0671ea5d33406f1aebf8e6e6302eb4e18cf2
santanaIgot <santanaigoor01@gmail.com>
2023-11-13 20:22:46 -0300

    Arquivo principal

---
+++ main.py
@@ -0,0 +1,15 @@
+def jogar():
+    palavra_secreta = "Banana"
+
+    enforcou = False
+    acertou = False
+    while not enforcou and not acertou:
+        print("Jogando")
+        break
+
+    
+    
+
+
+
+jogar()
