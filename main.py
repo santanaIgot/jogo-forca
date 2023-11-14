de64c491439b38496055085c189e55edcf216c3c
santanaIgot <santanaigoor01@gmail.com>
2023-11-13 21:23:37 -0300

    Arquivo principal

--- main.py
+++ main.py
@@ -4,10 +4,20 @@ def jogar():
     enforcou = False
     acertou = False
     while not enforcou and not acertou:
-        print("Jogando")
+        
+        chute = input("Qual letra ?")
+        chute = chute.strip()
+        # Criamos essa estrutura de repeticao para a variavel letra ir percorrendo as str e printalas depois da estrutura de condicao 
+        index = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                print('Encontrei a letra {} na posião {}'.format(letra,index))
+            index = index + 1
+
+        print("Jogando...")
         break
 
-    
+
     
 
 
